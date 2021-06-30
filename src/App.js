c9c829fed3030bb30443b961eafcaf5fdb9b86e6
beluCorvalan <bcorvalan.amil@gmail.com>
2021-06-29 21:58:43 -0300

    structure firebase conextion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import firebase from "firebase/app";
+import "@firebase/firestore";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCrrydG2OQmV5g4KO9GISzFBQPpn_TTSOU",
+  authDomain: "coderhouse-55b48.firebaseapp.com",
+  projectId: "coderhouse-55b48",
+  storageBucket: "coderhouse-55b48.appspot.com",
+  messagingSenderId: "1092113434811",
+  appId: "1:1092113434811:web:a0a039ddd9372e8aefc19e",
+});
+
+function getFirebase() {
+  return app;
+}
+function getFireStore() {
+  return firebase.firestore(app);
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+  setLoading(true)
+  const db =  getFireStore()
+  const itemCollection = db.collection("items")
+  itemCollection.get().then(
+    (querySnapshot)=>{
+      if(querySnapshot.size === 0){
+        console.log('no hay resultados')
+      }
+      setItems(querySnapshot.docs.map(doc => doc.data()))
+    }
+  ).catch((error)=>{console.log('Error searching items', error)}).finally(()=>{setLoading(false)})
+
+  }, []);
+  return <div className="App">
+    {console.log(items)}
+  </div>;
 }
 
 export default App;
