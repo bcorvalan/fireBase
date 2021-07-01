e9a246fcd1986908056217c628c28a872ad79df5
beluCorvalan <bcorvalan.amil@gmail.com>
2021-06-30 21:19:45 -0300

    before branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import React, { useEffect, useState } from "react";
-import firebase from "firebase/app";
-import "@firebase/firestore";
-
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyCrrydG2OQmV5g4KO9GISzFBQPpn_TTSOU",
-  authDomain: "coderhouse-55b48.firebaseapp.com",
-  projectId: "coderhouse-55b48",
-  storageBucket: "coderhouse-55b48.appspot.com",
-  messagingSenderId: "1092113434811",
-  appId: "1:1092113434811:web:a0a039ddd9372e8aefc19e",
-});
-
-function getFirebase() {
-  return app;
-}
-function getFireStore() {
-  return firebase.firestore(app);
-}
-
+import {getFireStore} from './firebase'
 
 function App() {
   const [items, setItems] = useState([])
@@ -37,8 +19,9 @@ function App() {
   ).catch((error)=>{console.log('Error searching items', error)}).finally(()=>{setLoading(false)})
 
   }, []);
-  return <div className="App">
-    {console.log(items)}
+
+return <div className="App">
+ 
   </div>;
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "@firebase/firestore";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCrrydG2OQmV5g4KO9GISzFBQPpn_TTSOU",
+  authDomain: "coderhouse-55b48.firebaseapp.com",
+  projectId: "coderhouse-55b48",
+  storageBucket: "coderhouse-55b48.appspot.com",
+  messagingSenderId: "1092113434811",
+  appId: "1:1092113434811:web:a0a039ddd9372e8aefc19e",
+});
+
+export function getFirebase() {
+  return app;
+}
+export function getFireStore() {
+  return firebase.firestore(app);
+}
